9424eb2872dcd6c745d2a5eb400232c28b862b72
cdn64 <gitcdn@gmail.com>
2023-01-07 21:06:50 +0100

    chore: move Layout to new components dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app"
 
 import "../styles/globals.css"
-import { Layout } from "../Layout"
+import { Layout } from "../components/Layout"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
