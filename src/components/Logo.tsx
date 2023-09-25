6ca65ab7181aad37cfddc769a520fee3070a8a6a
cdn64 <gitcdn@gmail.com>
2023-09-25 14:27:31 +0200

    fix: correct JSX in logo SVG

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,58 +7,58 @@ export const Logo = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_877_185)">
+    <g clipPath="url(#clip0_877_185)">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M0 388H584V439H0V388Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M0 291H584V342H0V291Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M0 194H584V245H0V194Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M0 97H584V148H0V97Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M0 0H584V51H0V0Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M60 0L60 236L-1.03159e-05 236L0 -2.62268e-06L60 0Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M584 202L584 438L524 438L524 202L584 202Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M60 303L60 438L-1.78991e-07 438L5.72205e-06 303L60 303Z"
         fill="black"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M584 0L584 135L524 135L524 -2.62268e-06L584 0Z"
         fill="black"
       />
